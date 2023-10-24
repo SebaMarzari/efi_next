80071e28b212c5d1a1d1ccdb81aa0a1ee847e826
Melisa Moreno <meli.moreno419@gmail.com>
2023-10-24 02:47:38 -0300

    Page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,36 @@
+import React from 'react';
+// Styles
+import './style.css'
 
 export default function App() {
   return (
-    <h1>Hello World!!</h1>
-  )
+    <div className='content-index'>
+      <header>
+        <img className='imgLogo' src="/images/logo.png" alt="Logo del Grupo" />
+      </header>
+      <main>
+        <h1 className='title'>Automatización del Modelado de Bases de Datos con NextJS y Sequelize</h1>
+
+        <p className='texto'>
+          En la era actual, impulsada por avances tecnológicos, la necesidad de crear herramientas
+          que aumenten la productividad en el desarrollo de sistemas es más evidente que nunca. 
+          Para abordar esta demanda, hemos concebido un sistema en NextJS que revolucionará la 
+          creación de modelos de bases de datos.
+        </p>
+
+        <p className='texto'>
+          Nuestro objetivo es simplificar todo el proceso de modelado, desde la creación de modelos
+          hasta la generación automática de archivos Model.js en Sequelize. A través de sencillos 
+          formularios, podrás definir modelos, agregar propiedades con detalles personalizados, 
+          establecer relaciones entre modelos y hasta crear índices únicos o no.
+        </p>
+
+        <p className='texto-invitacion'>
+          ¡Ingresa a nuestra plataforma y aumenta tu productividad en el desarrollo de bases de datos!
+        </p>
+
+        <img className='imgDB' src="/images/database.webp" alt="Logo del Grupo" />
+      </main>
+    </div>
+  );
 }
