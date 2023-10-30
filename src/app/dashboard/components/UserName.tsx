1e32686ffd4bdc6c6242c9ecf3b36256eaa2cadb
Melisa Moreno <meli.moreno419@gmail.com>
2023-10-30 13:31:29 -0300

    Vista de nombre de usuario en dashboard. (Falla en permisos, falta solucionar)

--- src/app/dashboard/components/UserName.tsx
+++ src/app/dashboard/components/UserName.tsx
@@ -3,16 +3,25 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const UserName = () => {
-  const [userName, setUserName] = useState("");
+  const [userName, setUserName] = useState("Usuario");
 
   useEffect(() => {
-    // Llamar al endpoint para obtener el nombre de usuario
-    axios.get("/api/models/fields?tableName=users").then((response) => {
-      setUserName(response.data.name);
-    });
+    // Llama al endpoint para obtener el nombre de usuario
+    axios.get("/api/models/fields?tableName=users")
+      .then((response) => {
+        if (response.data.name) {
+          setUserName(response.data.name);
+        } else {
+          // Si no encuentra el nombre, mantiene el nombre predeterminado
+          console.warn("El nombre de usuario no se encontró en la respuesta.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error al obtener el nombre de usuario:", error);
+      });
   }, []);
 
-  return <h1>{userName}</h1>;
+  return <h2>{userName}</h2>;
 };
 
 export default UserName;
