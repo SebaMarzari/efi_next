2b5e30779ffcab60558b6df1bb6a4710730931b6
Melisa Moreno <meli.moreno419@gmail.com>
2023-10-30 15:15:12 -0300

    Solucionada la vista del nombre de usuario en dashboard

--- src/app/dashboard/components/UserName.tsx
+++ src/app/dashboard/components/UserName.tsx
@@ -1,24 +1,37 @@
 "use client"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { getCookie, deleteCookie } from "@/functions/cookies";
+import { getBasicRequestConfig } from "@/functions/getRequestConfig";
+
+interface DecodedToken extends JwtPayload {
+  user_id: string;
+}
 
 const UserName = () => {
-  const [userName, setUserName] = useState("Usuario");
+  const [userName, setUserName] = useState<string | null>(null);
 
   useEffect(() => {
-    // Llama al endpoint para obtener el nombre de usuario
-    axios.get("/api/models/fields?tableName=users")
-      .then((response) => {
-        if (response.data.name) {
-          setUserName(response.data.name);
-        } else {
-          // Si no encuentra el nombre, mantiene el nombre predeterminado
-          console.warn("El nombre de usuario no se encontró en la respuesta.");
+    const getUser = async () => {
+      const alreadyLoggedUser = getCookie('token');
+      try {
+        if (alreadyLoggedUser) {
+          const decoded = jwt.decode(alreadyLoggedUser) as DecodedToken;
+
+          if (decoded && decoded.user_id) {
+            const config = getBasicRequestConfig(alreadyLoggedUser);
+            const response = await axios.get(`/api/user?id=${decoded.user_id}`, config);
+            const user = response.data.user;
+            setUserName(user.name);
+          }
         }
-      })
-      .catch((error) => {
-        console.error("Error al obtener el nombre de usuario:", error);
-      });
+      } catch (error) {
+        console.error("Error while fetching user data:", error);
+      }
+    };
+
+    getUser();
   }, []);
 
   return <h2>{userName}</h2>;
